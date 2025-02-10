bcf59c25fd288f842caf32d19c0814a8b01d4966
brendanw <brendan@basebeta.com>
2025-02-09 21:39:28 -0700

    expand unit test coverage

--- exitratings/src/commonTest/kotlin/WingsuitRatingTests.kt
+++ exitratings/src/commonTest/kotlin/WingsuitRatingTests.kt
@@ -11,7 +11,7 @@ import kotlin.test.assertNotEquals
 class WingsuitRatingTests {
 
    @Test
-   fun `dummy test`() = runTest {
+   fun `no rating generated for exit without laser profile`() = runTest {
       val rating = assembleWingsuitRating(
          exitProfile = emptyList(),
          flyableAltitude = 1000,
@@ -51,8 +51,130 @@ class WingsuitRatingTests {
          hmsl = 1462
       )
 
-      assertNotEquals(rating?.rating, null)
-      assertEquals(rating?.rating, JumpRating.SingleBlue.rating)
+      assertEquals(
+         expected = JumpRating.SingleBlue.rating,
+         actual = rating?.rating
+      )
+   }
+
+   /**
+    * This exit feels more visual than a single blue...probably because heading performance matters here
+    * That calls for the need for a yes/no question to bump a rating up
+    * End of ledge 1 is at 16m,-74m
+    * End of ledge 2 is at 47m,-164m
+    */
+   @Test
+   fun `Romsdalseggen`() = runTest {
+      val romsdalseggen = listOf(
+         Point(1, -22),
+         Point(3, -48),
+         Point(4, -48),
+         Point(5, -49),
+         Point(10, -59),
+         Point(12, -74),
+         Point(14, -74),
+         Point(16, -74),
+         Point(47, -164),
+         Point(51, -222),
+         Point(54, -223),
+         Point(57, -232),
+         Point(63, -240),
+         Point(68, -240),
+         Point(69, -243),
+         Point(85, -239),
+         Point(88, -239),
+         Point(92, -241)
+      )
+
+      val rating = assembleWingsuitRating(
+         exitProfile = romsdalseggen,
+         flyableAltitude = 1116,
+         minimumRequiredGlide = 2.0,
+         hmsl = 1125
+      )
+
+      assertEquals(
+         expected = JumpRating.SingleBlue.rating,
+         actual = rating?.rating
+      )
+   }
+
+   @Test
+   fun `Nunn -- Mid Mountain`() = runTest {
+      val nunnProfile = listOf(
+         Point(0, 0),
+         Point(1, -38),
+         Point(3, -47),
+         Point(8, -84),
+         Point(9, -84),
+         Point(10, -85),
+         Point(13, -97),
+         Point(15, -99),
+         Point(23, -134),
+         Point(29, -134),
+         Point(51, -159),
+         Point(59, -166),
+         Point(89, -187),
+      )
+
+      val rating = assembleWingsuitRating(
+         exitProfile = nunnProfile,
+         flyableAltitude = 1020,
+         minimumRequiredGlide = 2.5,
+         hmsl = 2450
+      )
+
+      assertEquals(
+         expected = JumpRating.DoubleBlue.rating,
+         actual = rating?.rating
+      )
+   }
+
+   @Test
+   fun `Mt Buller`() = runTest {
+      val buller = listOf(
+         Point(0, 0),
+         Point(6, -123),
+         Point(7, -123),
+         Point(11, -132),
+         Point(16, -152),
+         Point(19, -165),
+         Point(25, -177),
+         Point(28, -180),
+         Point(29, -179),
+         Point(36, -186),
+         Point(42, -191),
+         Point(45, -189),
+         Point(85, -325),
+         Point(116, -332),
+         Point(121, -334),
+         Point(125, -346),
+         Point(157, -375),
+         Point(169, -381),
+         Point(179, -386),
+         Point(179, -394),
+         Point(195, -407),
+         Point(212, -417),
+         Point(236, -432),
+         Point(245, -459),
+         Point(260, -461),
+         Point(284, -478),
+         Point(307, -496),
+         Point(340, -532),
+         Point(467, -603)
+      )
+
+      val rating = assembleWingsuitRating(
+         exitProfile = buller,
+         flyableAltitude = 1100,
+         minimumRequiredGlide = 2.65,
+         hmsl = 2802
+      )
+
+      assertEquals(
+         expected = JumpRating.SingleBlack.rating,
+         actual = rating?.rating
+      )
    }
 
    @Test
@@ -81,6 +203,49 @@ class WingsuitRatingTests {
       )
 
       assertEquals(expected = JumpRating.DoubleBlack.rating, actual = rating?.rating)
+   }
+
+   @Test
+   fun `Cascade`() = runTest {
+      val cascade = listOf(
+         Point(0, 0),
+         Point(5, -25),
+         Point(32, -143),
+         Point(89, -208),
+         Point(146, -258)
+      )
+
+      val rating = assembleWingsuitRating(
+         exitProfile = cascade,
+         flyableAltitude = 1801,
+         minimumRequiredGlide = 2.5,
+         hmsl = 2635
+      )
+
+      assertEquals(expected = JumpRating.DoubleBlack.rating, actual = rating?.rating)
+   }
+
+   @Test
+   fun `Nunn -- High`() = runTest {
+      val highNunnProfile = listOf(
+         Point(0, 0),
+         Point(12, -55),
+         Point(20, -97),
+         Point(25, -111),
+         Point(34, -120),
+         Point(62, -142),
+         Point(87, -147),
+         Point(126, -203),
+      )
+
+      val rating = assembleWingsuitRating(
+         exitProfile = highNunnProfile,
+         flyableAltitude = 1801,
+         minimumRequiredGlide = 2.5,
+         hmsl = 2635
+      )
+
+      assertEquals(expected = JumpRating.Red.rating, actual = rating?.rating)
    }
 
 }
